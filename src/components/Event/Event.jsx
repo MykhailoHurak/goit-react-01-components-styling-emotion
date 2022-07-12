3ed732a8f2d57364d990dab8169f490b0766684b
mikegurak <92752760+mikegurak@users.noreply.github.com>
2022-07-12 20:40:56 +0300

    @emotion

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
-import css from './Event.module.css';
 import { iconSize } from 'constants';
+import { Card, EventName, Info, Chip } from './Event.styled';
 // import { formatEventStart } from 'utilsHelpers/formatEventStart';
 // import { formatEventDuration } from 'utilsHelpers/formatEventDuration';
 import { formatEventStart, formatEventDuration } from 'utilsHelpers';
@@ -10,28 +10,30 @@ export const Event = ({ name, location, speaker, type, start, end }) => {
     const formatedStart = formatEventStart(start);
     const duration = formatEventDuration(start, end);
     return (
-        <div className={css.event}>
-            <h2 className={css.title}>{name}</h2>
-            <p className={css.info}>
-                <FaMapMarkerAlt className={css.icon} size={iconSize.sm} />
+        <Card>
+            <EventName>{name}</EventName>
+            <Info>
+                <FaMapMarkerAlt size={iconSize.sm} />
                 {location}
-            </p>
-            <p className={css.info}>
-                <FaUserAlt className={css.icon} size={iconSize.sm} />
+            </Info>
+            <Info>
+                <FaUserAlt size={iconSize.sm} />
                 {speaker}
-            </p>
-            <p className={css.info}>
-                <FaCalendarAlt className={css.icon} size={iconSize.sm} />
+            </Info>
+            <Info>
+                <FaCalendarAlt size={iconSize.sm} />
                 {formatedStart}
-            </p>
-            <p className={css.info}>
-                <FaClock className={css.icon} size={iconSize.sm} />
+            </Info>
+            <Info>
+                <FaClock size={iconSize.sm} />
                 {duration}
-            </p>
-            <span className={`${css.chip} ${css[type]}`}>{type}</span>
-        </div>
+            </Info>
+            <Chip eventType={type}>{type}</Chip>
+        </Card>
     );
 };
+
+// className={`${css.chip} ${css[type]}`}
 
 Event.prototype = {
     name: PropTypes.string.isRequired,
